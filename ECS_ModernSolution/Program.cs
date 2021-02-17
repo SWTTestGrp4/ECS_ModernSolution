7c05fe0011591158b5c4c106661caf4d29752305
Jesper Trøiborg <jespertroiborg@hotmail.com>
2021-02-17 09:39:18 +0100

    Add project files.

---
+++ ECS_ModernSolution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ECS_ModernSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
